d29e4cb64b1e3f72a076407d2f8e3feed77244da
hikod <hikmetdogrul00@gmail.com>
2018-03-15 16:33:43 -0400

    Second push

--- src/test/java/com/trulia/pages/HomePage.java
+++ src/test/java/com/trulia/pages/HomePage.java
@@ -1,6 +1,7 @@
 package com.trulia.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
@@ -10,6 +11,12 @@ private WebDriver driver;
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
+	}
+	
+	
+	
+	public boolean isAt() {
+		return driver.getTitle().equals("Trulia: Real Estate Listings, Homes For Sale, Housing Data");
 	}
 	
 }
--- src/test/java/com/trulia/tests/LoginTest.java
+++ src/test/java/com/trulia/tests/LoginTest.java
@@ -1,7 +1,20 @@
 package com.trulia.tests;
 
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+import com.trulia.pages.HomePage;
 import com.trulia.utilities.TestBase;
 
 public class LoginTest extends TestBase {
 
+	@Test
+	public void homePageTitle() {
+		// create object from Page class:
+		HomePage homePage = new HomePage(driver);
+		// verify at page
+		assertTrue(homePage.isAt());
+
+	}
 }
